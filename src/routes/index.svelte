925895e907bd8f9c2a2632ddd29aeb1d246cf74e
jsborbon <santiagoborbon@gmail.com>
2024-11-08 12:38:10 +0100

    Examples added updated

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { goto } from '$app/navigation'; // Importa correctamente
+
+    // Función para navegar a la página del contador
+    const navigateToCounter = () => {
+        goto('/counter'); // Asegúrate de que la ruta sea correcta
+    };
+</script>
+
+<h1>Página de Inicio</h1>
+<h1><a href="/counter">Counter</a></h1>
+<button on:click={navigateToCounter}>Ir a Counter</button>
